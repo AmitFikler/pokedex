def0ef4e0dc39c46e1c0162910b2db9845b3e0a1
AmitFikler <amitfikler@gmail.com>
2021-11-02 17:09:13 +0200

    deployment

---
+++ index.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const app = express();
+const cors = require('cors')
+const pokemonRouter = require("./src/routers/pokemonRouter")
+const userRouter = require('./src/routers/userRouter')
+const handleUser = require("./src/middleware/userHandler")
+const errorHandle = require("./src/middleware/errorHandler")
+const path = require("path")
+const port = 8080;
+
+
+
+app.use(cors())
+
+app.use(express.json())
+app.use('/pokemon',handleUser,pokemonRouter)
+app.use('/info', userRouter)
+app.use(errorHandle)
+
+app.use(express.static(path.resolve('./dist'))); // serve main path as static dir
+app.get('/', function(req, res) { // serve main path as static file
+  res.sendFile(path.resolve('./dist/index.html'))
+});
+// start the server
+app.listen(port, function() {
+  console.log('app started');
+});
+
+// route our app
+app.get('/', function(req, res) {
+  res.send('hello world!');
+});
+
+
+// P.getPokemonByName(34, function(response, error) { // with callback
+//     if(!error) {
+//       console.log(response);
+//     } else {
+//       console.log(error)
+//     }
+//   });
